265d9b3e0cb73641aa9dcc1d2b3cafe37df22941
Bruno Coimbra <coimbra@live.com>
2023-08-21 13:03:42 -0500

    Update dev-tools installation scripts for Alma 9

--- gwms-dev-setup/install_factory.sh
+++ gwms-dev-setup/install_factory.sh
@@ -11,11 +11,16 @@ FACTORY_XML_TEMPLATE="$DIR"/Templates/glideinWMS.xml
 FACTORY_MAPFILE=/etc/condor/certs/condor_mapfile
 FACTORY_MAPFILE_TEMPLATE="$DIR"/Templates/factory_condor_mapfile
 TOKEN_DIR=/var/lib/gwms-factory/.condor/tokens.d
-CONDOR_TARBALL_VERSION=9.0.0
 CONDOR_TARBALL_PATH=/var/lib/gwms-factory/condor
 OSG_REPO=osg
-FACTORY_REPO=osg
+FACTORY_REPO=osg-development
+OSG_VERSION=3.6
+OS_VERSION=9
 EXTRA_REPOS=
+
+CONDOR_TARBALL_URLS='
+https://research.cs.wisc.edu/htcondor/tarball/9.0/9.0.17/release/condor-9.0.17-x86_64_CentOS7-stripped.tar.gz
+'
 
 # Argument parser
 while [ -n "$1" ];do
@@ -32,11 +37,11 @@ while [ -n "$1" ];do
             FRONTEND_HOST="$2"
             shift
             ;;
-        --osg36)
-            OSG_3_6="1"
+        --osg-version)
+            OSG_VERSION="$2"
             ;;
-        --el8)
-            EL8="1"
+        --os-version)
+            OS_VERSION="$2"
             ;;
         -y)
             Y="-y"
@@ -54,101 +59,80 @@ if [ -z "$FRONTEND_HOST" ]; then
     exit 2
 fi
 
-# Process variables
-CONDOR_TARBALL_URL=https://research.cs.wisc.edu/htcondor/tarball/
-CONDOR_TARBALL_URL="$CONDOR_TARBALL_URL"$(echo "$CONDOR_TARBALL_VERSION" | cut -d . -f 1,2)/
-CONDOR_TARBALL_URL="$CONDOR_TARBALL_URL""$CONDOR_TARBALL_VERSION"/release/
-CONDOR_TARBALL_URL="$CONDOR_TARBALL_URL"condor-"$CONDOR_TARBALL_VERSION"-x86_64_CentOS7-stripped.tar.gz
-CONDOR_TARBALL_DIR=condor-"$CONDOR_TARBALL_VERSION"
-if [ "$(echo $CONDOR_TARBALL_VERSION | cut -d '.' -f 1)" -ge "9" ]; then
-    CONDOR_TARBALL_DIR="$CONDOR_TARBALL_DIR"-1
-fi
-CONDOR_TARBALL_DIR="$CONDOR_TARBALL_DIR"-x86_64_CentOS7-stripped
-CONDOR_TARBALL=condor-"$CONDOR_TARBALL_VERSION"-x86_64_CentOS7-stripped
+# Export variables
 export FACTORY_HOST
 export FRONTEND_HOST
-export CONDOR_TARBALL
-export CONDOR_TARBALL_DIR
-export CONDOR_TARBALL_PATH
-export CONDOR_TARBALL_VERSION
 
 
 ### REPOSITORIES ###
 
 # EPEL Repo
-if [ -z "$EL8" ]; then
-    yum install $Y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+if [ "$OS_VERSION" -le 7 ]; then
+    yum install "$Y" https://dl.fedoraproject.org/pub/epel/epel-release-latest-"$OS_VERSION".noarch.rpm
 else
     # Alma8 requires powertools (alma8-powertools)
     EXTRA_REPOS="$EXTRA_REPOS --enablerepo=powertools"
 fi
 
 # OSG Repo
-if [ -z "$OSG_3_6" ]; then
-    OSG_VERSION=3.5
-else
-    OSG_VERSION=3.6
-fi
-if [ -z "$EL8" ]; then
-    EL_VERSION=el7
-else
-    EL_VERSION=el8
-fi
-yum install $Y https://repo.opensciencegrid.org/osg/"$OSG_VERSION"/osg-"$OSG_VERSION"-"$EL_VERSION"-release-latest.rpm
+yum install "$Y" https://repo.opensciencegrid.org/osg/"$OSG_VERSION"/osg-"$OSG_VERSION"-el"$OS_VERSION"-release-latest.rpm
 sed -i "s/priority=[0-9]*/priority=1/g" /etc/yum.repos.d/osg*
 
 
 ### INSTALLATION ###
 
-
-
 # OSG
-yum install $Y osg-ca-certs $EXTRA_REPOS --enablerepo="$OSG_REPO"
+yum install "$Y" osg-ca-certs "$EXTRA_REPOS" --enablerepo="$OSG_REPO"
 
 # HTCondor
-yum install $Y condor.x86_64 $EXTRA_REPOS --enablerepo="$OSG_REPO" 
+if [ "$OS_VERSION" -gt 7 ]; then
+    yum install "$Y" condor.x86_64 "$EXTRA_REPOS" --enablerepo="$OSG_REPO"
+else
+    yum install "$Y" https://research.cs.wisc.edu/htcondor/repo/9.0/htcondor-release-current.el7.noarch.rpm
+    yum install "$Y" condor --disablerepo=osg*
+fi
 
 # Factory
-yum install $Y glideinwms-factory $EXTRA_REPOS --enablerepo="$FACTORY_REPO"
+yum install "$Y" glideinwms-factory "$EXTRA_REPOS" --enablerepo="$FACTORY_REPO"
 
 
 ### CONFIGURATION ###
 
 # glideinWMS.xml
-mv $FACTORY_XML $FACTORY_XML.bak
+rm -f $FACTORY_XML
 envsubst < "$FACTORY_XML_TEMPLATE" > "/tmp/glideinWMS.xml"
 mv "/tmp/glideinWMS.xml" "$FACTORY_XML"
 chown gfactory.gfactory "$FACTORY_XML"
 echo Updated "$FACTORY_XML"
 
 # condor_mapfile
-mv $FACTORY_MAPFILE $FACTORY_MAPFILE.bak
+rm -f $FACTORY_MAPFILE
 envsubst < "$FACTORY_MAPFILE_TEMPLATE" > "/tmp/condor_mapfile"
 mv "/tmp/condor_mapfile" "$FACTORY_MAPFILE"
 echo Updated "$FACTORY_MAPFILE"
 
 # Tokens
 echo Creating IDTOKENS...
-systemctl start condor
-sleep 5
 mkdir -p "$TOKEN_DIR"
+condor_store_cred -c add -p glideinwms-pass-$RANDOM
 condor_token_create -id gfactory@"$HOSTNAME" > "$TOKEN_DIR"/gfactory."$HOSTNAME".idtoken
 chown -R gfactory:gfactory "$TOKEN_DIR"
 chmod 600 "$TOKEN_DIR"/*
 condor_token_create -id vofrontend_service@"$HOSTNAME" -key POOL > ~/frontend."$HOSTNAME".idtoken
 ls -lah "$TOKEN_DIR"
-systemctl stop condor
 
 # Condor tarball
 [ ! -d $CONDOR_TARBALL_PATH ] && mkdir -p $CONDOR_TARBALL_PATH
 pushd $CONDOR_TARBALL_PATH || exit 3
-wget "$CONDOR_TARBALL_URL"
-tar -xf "$CONDOR_TARBALL".tar.gz && rm "$CONDOR_TARBALL".tar.gz
+for CONDOR_TARBALL_URL in $CONDOR_TARBALL_URLS; do
+    wget "$CONDOR_TARBALL_URL"
+    CONDOR_TARBALL=$(echo "$CONDOR_TARBALL_URL" | awk -F'/' '{print $NF}')
+    tar -xf "$CONDOR_TARBALL" && rm -f "$CONDOR_TARBALL"
+done
 popd || exit 4
 
 # Update factory configuration
 gwms-factory upgrade
-gwms-factory reconfig
 
 
 ### START SERVICES ###
--- gwms-dev-setup/install_frontend.sh
+++ gwms-dev-setup/install_frontend.sh
@@ -13,12 +13,15 @@ FRONTEND_MAPFILE=/etc/condor/certs/condor_mapfile
 FRONTEND_MAPFILE_TEMPLATE="$DIR"/Templates/frontend_condor_mapfile
 CERT="$DIR"/Certificates/usercert.pem
 KEY="$DIR"/Certificates/userkey.pem
-PROXY_DIR=/proxy
+CRED_DIR=/opt/gwms/credentials
 TOKEN_DIR=/var/lib/gwms-frontend/.condor/tokens.d
-VOFE_PROXY="$PROXY_DIR"/vofe_proxy
-PILOT_PROXY="$PROXY_DIR"/pilot_proxy
+VOFE_PROXY="$CRED_DIR"/vofe_proxy
+PILOT_PROXY="$CRED_DIR"/pilot_proxy
+PILOT_SCITOKEN="$CRED_DIR"/pilot.scitoken
 OSG_REPO=osg
-FRONTEND_REPO=osg
+FRONTEND_REPO=osg-development
+OSG_VERSION=3.6
+OS_VERSION=9
 EXTRA_REPOS=
 
 # Argument parser
@@ -44,11 +47,11 @@ while [ -n "$1" ];do
             KEY="$2"
             shift
             ;;
-        --osg36)
-            OSG_3_6="1"
+        --osg-version)
+            OSG_VERSION="$2"
             ;;
-        --el8)
-            EL8="1"
+        --os-version)
+            OS_VERSION="$2"
             ;;
         -y)
             Y="-y"
@@ -74,69 +77,66 @@ export FACTORY_HOST
 export PILOT_DN
 export VOFE_PROXY
 export PILOT_PROXY
+export PILOT_SCITOKEN
 export TOKEN_DIR
 
 
 ### REPOSITORIES ###
 
 # EPEL Repo
-if [ -z "$EL8" ]; then
-    yum install $Y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+if [ "$OS_VERSION" -le 7 ]; then
+    yum install "$Y" https://dl.fedoraproject.org/pub/epel/epel-release-latest-"$OS_VERSION".noarch.rpm
 else
     # Alma8 requires powertools (alma8-powertools)
     EXTRA_REPOS="$EXTRA_REPOS --enablerepo=powertools"
 fi
 
 # OSG Repo
-if [ -z "$OSG_3_6" ]; then
-    OSG_VERSION=3.5
-else
-    OSG_VERSION=3.6
-fi
-if [ -z "$EL8" ]; then
-    EL_VERSION=el7
-else
-    EL_VERSION=el8
-fi
-yum install $Y https://repo.opensciencegrid.org/osg/"$OSG_VERSION"/osg-"$OSG_VERSION"-"$EL_VERSION"-release-latest.rpm
+yum install "$Y" https://repo.opensciencegrid.org/osg/"$OSG_VERSION"/osg-"$OSG_VERSION"-el"$OS_VERSION"-release-latest.rpm
 sed -i "s/priority=[0-9]*/priority=1/g" /etc/yum.repos.d/osg*
 
 
 ### INSTALLATION ###
 
 # OSG
-yum install $Y osg-ca-certs $EXTRA_REPOS --enablerepo="$OSG_REPO"
+yum install "$Y" osg-ca-certs $EXTRA_REPOS --enablerepo="$OSG_REPO"
 
 # Token Tools
-yum install $Y htgettoken $EXTRA_REPOS --enablerepo="$OSG_REPO"
+yum install "$Y" htgettoken $EXTRA_REPOS --enablerepo="$OSG_REPO"
 
 # HTCondor
-yum install $Y condor.x86_64 $EXTRA_REPOS --enablerepo="$OSG_REPO"
+if [ "$OS_VERSION" -gt 7 ]; then
+    yum install "$Y" condor.x86_64 "$EXTRA_REPOS" --enablerepo="$OSG_REPO"
+else
+    yum install "$Y" https://research.cs.wisc.edu/htcondor/repo/9.0/htcondor-release-current.el7.noarch.rpm
+    yum install "$Y" condor --disablerepo=osg*
+fi
 
 # Frontend
-yum install $Y glideinwms-vofrontend $EXTRA_REPOS --enablerepo="$FRONTEND_REPO"
+yum remove "$Y" voms
+yum install "$Y" glideinwms-vofrontend $EXTRA_REPOS --enablerepo="$FRONTEND_REPO" --disablerepo=slf-primary
 
 # Fermi extra
-yum install $Y fermilab-util_kx509
+yum install "$Y" fermilab-util_kx509
 
 ### CONFIGURATION ###
 
 # frontend.xml
-mv $FRONTEND_XML $FRONTEND_XML.bak
+rm -f $FRONTEND_XML
 envsubst < "$FRONTEND_XML_TEMPLATE" > "/tmp/frontend.xml"
 mv "/tmp/frontend.xml" "$FRONTEND_XML"
 chown frontend.frontend "$FRONTEND_XML"
 echo Updated "$FRONTEND_XML"
 
 # condor_mapfile
-mv $FRONTEND_MAPFILE $FRONTEND_MAPFILE.bak
+rm -f $FRONTEND_MAPFILE
 envsubst < "$FRONTEND_MAPFILE_TEMPLATE" > "/tmp/condor_mapfile"
 mv "/tmp/condor_mapfile" "$FRONTEND_MAPFILE"
 echo Updated "$FRONTEND_MAPFILE"
 
 # Proxies
 echo Creating proxies...
-mkdir -p "$PROXY_DIR"
+mkdir -p "$CRED_DIR"
 voms-proxy-init -valid 900:00 -cert /etc/grid-security/hostcert.pem -key /etc/grid-security/hostkey.pem -out "$VOFE_PROXY"
 voms-proxy-init -debug -valid 900:00 -cert "$CERT" -key "$KEY" -out "$PILOT_PROXY" -voms fermilab
 chown frontend.frontend "$VOFE_PROXY"
@@ -146,21 +146,22 @@ ls -lah "$PILOT_PROXY"
 
 # Tokens
 echo Creating IDTOKENS...
-systemctl start condor
-sleep 5
 mkdir -p "$TOKEN_DIR"
+condor_store_cred -c add -p $RANDOM
 condor_token_create -id vofrontend_service@"$HOSTNAME" -key POOL > "$TOKEN_DIR"/frontend."$HOSTNAME".idtoken
 scp root@"$FACTORY_HOST":/root/frontend."$FACTORY_HOST".idtoken  "$TOKEN_DIR"/frontend."$FACTORY_HOST".idtoken
-systemctl stop condor
-echo Generating SciToken...
-htgettoken --minsecs=3580 -i fermilab -v -a fermicloud543.fnal.gov -o "$TOKEN_DIR"/"$HOSTNAME".scitoken
 chown -R frontend:frontend "$TOKEN_DIR"
 chmod 600 "$TOKEN_DIR"/*
 ls -lah "$TOKEN_DIR"
+echo Generating SciToken...
+mkdir -p "$CRED_DIR"
+htgettoken --minsecs=3580 -i fermilab -v -a htvaultprod.fnal.gov -o "$PILOT_SCITOKEN"
+chown frontend.frontend "$PILOT_SCITOKEN"
+chmod 600 "$PILOT_SCITOKEN"
+ls -lah "$PILOT_SCITOKEN"
 
 # Update frontend configuration
 gwms-frontend upgrade
-gwms-frontend reconfig
 
 
 ### START SERVICES ###
